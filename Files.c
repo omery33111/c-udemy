60e14016def27469953ad94ccd7146f10b3f4d75
Omer Yanai <omery33111@gmail.com>
2023-09-03 16:38:42 +0300

    Udemy Course Done

--- Files.c
+++ Files.c
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> // INPUT AND OUTPUT OPERATIONS
 #include <math.h> // MATHEMATICAL OPERATIONS
 #include <string.h> // STRING MANIPULATIONS
@@ -85,4 +84,194 @@
 //         printf("%c", myChar1);
 //         fclose(fp);
 //     }
+// }
+
+
+
+
+
+/* fputs() FUNCTION */
+
+// int main()
+// {
+//     FILE* fp;
+    
+//     fputs("Hey man", stdout);
+//     fputs("This is me! Omer!", stdout);
+
+//     fp = fopen("InitialFiles.txt", "w");
+
+//     if (fp != NULL)
+//     {   
+//         fputs("Hey man", fp);
+//         fputs("This is me! Omer!", fp);
+//         fclose(fp);
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+/* fgets() FUNCTION */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     char myString[10];
+//     int count = 0;
+    
+//     fp = fopen("InitialFiles.txt", "r");
+
+//     if (fp != NULL)
+//     {   
+//         while (fgets(myString, 10, fp))
+//             printf("String #%d read: %s", count++, myString);
+        
+//         fclose(fp);
+//     }
+// }
+
+
+
+
+
+/* CHARACTER LENGTH IN A FILE */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     int counter = 0;
+
+//     char myChar;
+//     fp = fopen("InitialFiles.txt", "r");
+
+//     if (fp != NULL)
+//     {   
+//         while ((myChar = fgetc(fp)) != EOF)
+//         {
+//             printf("%c", myChar);
+
+//             counter++;
+//         }
+
+//         fclose(fp);
+//     }
+
+//     printf("\nTotal of %d characters in the file!", counter);
+// }
+
+
+
+
+
+/* NUMBER OF SENTENCES IN A FILE */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     int counterLines = 1;
+
+//     char myChar;
+//     fp = fopen("InitialFiles.txt", "r");
+
+//     if (fp != NULL)
+//     {   
+//         while ((myChar = fgetc(fp)) != EOF)
+//         {
+//             if (myChar == '\n')
+//                 counterLines++;
+//         }
+
+//         fclose(fp);
+//     }
+
+//     printf("\nTotal of %d sentences in the file!", counterLines);
+// }
+
+
+
+
+
+/* POWER OF A NUMBER IN A FILE */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     fp = fopen("InitialFiles.txt", "w");
+
+//     if (fp != NULL)
+//     {   
+//         for (int i = 1; i <= 10; i++)
+//         {
+//             fprintf(fp, "%d %d\n", i, i * i);
+//         }
+//         fclose(fp);
+//     }
+// }
+
+
+
+
+
+/* fscanf() FUNCTION */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     int num1, num2;
+
+//     fp = fopen("InitialFiles.txt", "r");
+
+//     if (fp != NULL)
+//     {   
+//         for (int i = 1; i <= 10; i++)
+//         {
+//             fscanf(fp, "%d%d", &num1, &num2);
+//             printf("Read: %d %d\n", num1, num2);
+//         }
+
+//         fclose(fp);
+//     }
+// }
+
+
+
+
+
+/* CALCULATE CHARACTER APPEARANCES IN A FILE */
+
+// int main()
+// {
+//     FILE* fp;
+
+//     char desiredCharacter;
+//     int counter = 0;
+//     char fileName[20] = {0};
+
+//     printf("Enter desired character: ");
+//     scanf("%c", &desiredCharacter);
+
+//     printf("Enter file name to test: ");
+//     scanf("%s", fileName);
+
+//     fp = fopen(fileName, "r");
+
+//     if (fp != NULL)
+//     {   
+//         while (!feof(fp))
+//             if (fgetc(fp) == desiredCharacter)
+//                 counter++;
+
+//         fclose(fp);
+//     }
+
+//     printf("Total appearances of character %c is %d", desiredCharacter, counter);
 // }
